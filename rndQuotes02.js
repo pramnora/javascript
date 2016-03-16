912f2e315bb80ce6c127e5a3b5062524c45f3993
Paul Ramnora <pramnora@yahoo.com>
2016-03-16 22:35:26 +0000

    Create rndQuotes02.js
    Using the twin array method to store both names/quotes, separately.

---
+++ rndQuotes02.js
@@ -0,0 +1,15 @@
+
+var names = [
+             "Ali",
+             "Shakespeare",
+             "Nat King Cole"
+];
+var quotes = [
+              "\"I am the Greatest!\"",
+              "\"To be, or not to be...that is the ?\"",
+              "\"On a clear day one can live, forever...\""
+];
+
+for (var x=0;x<names.length;x++){
+ alert(quotes[x]+ " -" + names[x]);
+}
