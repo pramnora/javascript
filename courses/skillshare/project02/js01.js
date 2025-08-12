3780554b0106a52e2a2d0c0d31e7a4d50afa62cf
Paul Ramnora <pramnora@yahoo.com>
2025-08-12 18:56:39 +0100

    Update js01.js
    Updated file: [js01.js] Added function printFinalResultsScore

--- courses/skillshare/project02/js01.js
+++ courses/skillshare/project02/js01.js
@@ -1,6 +1,6 @@
 /* 
   Created: Tue 12 Aug 2025 15:06 PM GMT
-  Updated: Tue 12 Aug 2025 18:00 PM GMT
+  Updated: Tue 12 Aug 2025 18:55 PM GMT
 */
 
 // variable declarations...
@@ -16,35 +16,32 @@ function init(){
  totQuestions=document.getElementById("idDivTotalQuestions");
  totQuestions.innerText=totalQuestions;
 
- currQuestion=document.getElementById("idDivCurrentQuestion");
- currQuestion.innerText=currentQuestion;
-
  displayScores();
 }
 
 function displayScores(){
+
+ if (currentQuestion <= totalQuestions){
+  currQuestion=document.getElementById("idDivCurrentQuestion");
+  currQuestion.innerText=currentQuestion;
+ }
+
  correctScore=document.getElementById("idDivCorrectScore");
  correctScore.innerText=correct;
 
  wrongScore=document.getElementById("idDivWrongScore");
  wrongScore.innerText=wrong;
+
 }
 
 function check(num){
 
-
- alert("num="+num)
-
  selObj=document.getElementById("selQ"+num).value;
-
- alert("selObj="+selObj)
 
  if (num==1) {
    if (selObj=="London"){
-    alert('correct1');
     correct += 1;
    } else {
-    alert('wrong1');
     wrong += 1;
    }
    currentQuestion += 1;
@@ -52,10 +49,8 @@ function check(num){
 
  if (num==2) {
    if (selObj=="Charles"){
-    alert('correct2');
     correct += 1;
    } else {
-    alert('wrong2');
     wrong += 1;
    }
    currentQuestion += 1;
@@ -63,10 +58,8 @@ function check(num){
 
  if (num==3) {
    if (selObj=="France"){
-    alert('correct3');
     correct += 1;
    } else {
-    alert('wrong3');
     wrong += 1;
    }
    currentQuestion += 1;
@@ -74,22 +67,25 @@ function check(num){
 
  if (num==4) {
    if (selObj=="18"){
-    alert('correct4');
+
     correct += 1;
    } else {
-    alert('wrong4');
-    wrong += 1;
+      wrong += 1;
    }
    currentQuestion += 1;
  }
 
  displayScores();
+ printFinalScore();
 
- if (currentQuestion == totalQuestions+1) {
-  alert("End of quiz! Your score is: " + correct + " out of a possible " + totalQuestions + " answers.");
- }
 
 }
 
+function printFinalScore(){
+  if (currentQuestion > totalQuestions) {
+   x=document.getElementById("idDivFinalResultsScore"); 
+   x.innerHTML="<strong>-End of quiz!-</strong><br>Your score is: " + correct + " out of a possible " + totalQuestions + " correct answers.";
+  }
+ }
 
  
